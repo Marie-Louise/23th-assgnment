3db53b6a708da5926fd61460128f2d801fb8d9f5
Marie-Louise <mariemuteteyimana@gmail.com>
2021-03-10 22:40:43 +0300

    23th assgnment

---
+++ src/sara.kt
@@ -0,0 +1,43 @@
+fun main() {
+    name()
+     modulus()
+     sum()
+     personality()
+
+
+
+}
+fun name() {
+    var name="Marie"
+    println(name)
+}
+fun modulus(){
+    var num1=20
+    var num2=3
+    var modulus=num1%num2
+    println(modulus)
+}
+fun sum(){
+    var num1=6
+    var num2=4
+    var num3=8
+    var num4=2
+    var sum=num1+num2+num3+num4
+    println(sum)
+}
+fun personality(){
+    println("patient")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
